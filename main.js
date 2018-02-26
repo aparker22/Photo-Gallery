f7d9721a633b8e4d431871f2e57c981f21f8da98
Ashley Parker <aparker7698@att.net>
2018-02-26 09:17:15 -0500

    Updates 2-23-18

--- main.js
+++ main.js
@@ -1,58 +1,34 @@
 var container = document.querySelector('.container'); //This is a search for my class
 var images = ['beach1.jpeg', 'beach2.jpeg', 'beach3.jpeg', 'beach4.jpg']; //a list of all my images
-var images1 = []; // an emtpy array that the new image objects will be placed into
-
-
-//the loop that puts my images on the page
-var pictures = function(x) {
-    for (var i = 0; i < x.length; i++) {
-        var img = x[i];
-        img = document.createElement('img');
-        img.setAttribute("src", x[i]);
-        container.appendChild(img);
-        images1.push(img); //puts images in new array
-    }
-};
-pictures(images); //runs previous function
-
-
-
 var main = document.getElementById('currentImg');
-var divM = document.querySelector('#addmain');
-
-//to hide lightbox
-var hide = function (e) {
-    divM.classList.toggle('hidden');
-}
-
-
-
+var lightbox = document.querySelector('#lightbox');
+var right = document.querySelector('.ar');
+var left = document.querySelector('.al');
 
 //define function for what will happen on mouse click
 var alert = function (e) {
     var img = this.getAttribute("src");
     main.setAttribute("src", img);
-    divM.classList.remove('hidden');
-    divM.addEventListener('click', hide);
+    lightbox.classList.remove('hidden');
+    lightbox.addEventListener('click', hide);
     right.addEventListener('click', something);
     left.addEventListener('click', something);
 }
 
-//use loop to add mouseclick function to each image in array
-var clickPictures = function (x) {
-    for (var i = 0; i < x.length; i++) {
-        var img = x[i];
+//the loop that puts my images on the page and now adds a listener function
+images.forEach(function(i) {
+        img = document.createElement('img');
+        img.setAttribute("src", i);
+        container.appendChild(img);
         img.addEventListener('click', alert);
-    }
+});
+
+//to hide lightbox
+var hide = function (e) {
+    lightbox.classList.toggle('hidden');
 }
 
-//run function
-clickPictures(images1)
-
-//to set event Listener for arrow keys
-var right = document.querySelector('.ar');
-var left = document.querySelector('.al');
-
+//function for arrow clicks
 var something = function (e) {
     e.preventDefault();
     var target = e.target;
